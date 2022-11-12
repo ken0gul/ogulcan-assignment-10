3a9be6e16290580fd9e657cc7f22d65e9f3b5da7
TheJohnCodes <93669966+ken0gul@users.noreply.github.com>
2022-11-12 11:02:51 -0500

    Remove whitespaces

--- ogulcan-assignment-10/src/main/java/com/coderscampus/assignment10/dto/WeekResponse.java
+++ ogulcan-assignment-10/src/main/java/com/coderscampus/assignment10/dto/WeekResponse.java
@@ -4,40 +4,23 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class WeekResponse {
 	@JsonProperty("week")
-	private  Week week;
+	private Week week;
 
-	
-	
 	public WeekResponse() {
-		
+
 	}
-
-
 
 	public WeekResponse(Week week) {
 		super();
 		this.week = week;
 	}
 
-
-
 	public Week getWeek() {
 		return week;
 	}
 
-
-
 	public void setWeek(Week week) {
 		this.week = week;
 	}
-	
-	
-
-
-	
-
-
-
-	
 
 }
